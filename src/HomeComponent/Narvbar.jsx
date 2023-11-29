fde6e62dade568c928fdad4272e35cf5437e4edb
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-29 05:53:02 -0800

    latest

---
+++ src/HomeComponent/Narvbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from './Images/craves.png'
+
+
+// import 'Na'
+
+const Narvbar = () => {
+  return (
+    <div> 
+      <div className='navDiv'>
+        <div >
+          <img src={logo} alt="" />
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Narvbar
