bf7f92347f5fa605a2ede2e387b0c1948e61aa73
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-30 16:54:51 -0800

    latest

--- src/Components/HomeComponent/SectionOne.jsx
+++ src/Components/HomeComponent/SectionOne.jsx
@@ -4,10 +4,37 @@ import './SectionOne.scss'
 import { RiSearch2Line } from "react-icons/ri";
 
 import smallImg from './Images/back2.png'
+import sev1 from './Images/sev1.png'
+import sev2 from './Images/sev2.png'
+import sev3 from './Images/sev3.png'
+import sev4 from './Images/sev4.png'
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const SectionOne = () => {
+
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+
   return (
-    <div className='sectionOne'>
+    <div>
+            <div className='sectionOne'>
         <div className='sectionSubDiv'>
             <h2>Discover the Best <br /> Talents Across Africa</h2>
             <p className='pOne'>
@@ -30,6 +57,55 @@ const SectionOne = () => {
         <div className='smallImg'>
             <img src={smallImg} alt="" />
         </div>
+    </div>
+
+
+    <div className='caroselSection'>
+        <h2>Explore Popular <br /> Categories</h2>
+        <Carousel 
+            responsive={responsive}
+            swipeable={false}
+            draggable={true}
+            showDots={false}
+            infinite={true}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+        >
+            <div className='caroImageDiv'>
+                <div className='overlayDiv'>
+                    <img src={sev1} alt="" />
+                    <div class="overlay"></div>
+                </div>
+                <p>Frontend Development</p>
+            </div>
+
+            <div className='caroImageDiv'>
+                <div className='overlayDiv'>
+                    <img src={sev2} alt="" />
+                    <div class="overlay"></div>
+                </div>
+                <p>Graphic Design</p>
+            </div>
+
+            <div className='caroImageDiv'>
+                <div className='overlayDiv'>
+                    <img src={sev3} alt="" />
+                    <div class="overlay"></div>
+                </div>
+                <p>Electrician</p>
+            </div>
+
+            <div className='caroImageDiv'>
+                <div className='overlayDiv'>
+                    <img src={sev4} alt="" />
+                    <div class="overlay"></div>
+                </div>
+                <p>Fashion Design</p>
+            </div>
+
+        </Carousel>
+        <button className='allBtn'>All Services</button>
+    </div>
     </div>
   )
 }
