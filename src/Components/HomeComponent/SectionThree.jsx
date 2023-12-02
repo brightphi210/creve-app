25bcc5dd19dcc6c744a23f338e0b8db33cd4c4e8
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-12-02 07:10:24 -0800

    latest

--- src/Components/HomeComponent/SectionThree.jsx
+++ src/Components/HomeComponent/SectionThree.jsx
@@ -4,11 +4,12 @@ import './SectionThree.scss'
 import icon1 from './Images/icons (1).png'
 import icon2 from './Images/icons (2).png'
 import icon3 from './Images/icons (3).png'
+import becomeImg from './Images/becomeImg.png'
 
 const SectionThree = () => {
   return (
     <div className='sectionThree'>
-        <h2>How Creeve Works</h2>
+        <h2>How Creve Works</h2>
         <div className='sectionThreeFlex'>
             <div className='secThreeDiv'>
                 <img src={icon2} alt="" />
@@ -39,6 +40,21 @@ const SectionThree = () => {
                         Whether it’s skilled or unskilled, you are in the right place. 
                     </p>
                 </div>
+            </div>
+        </div>
+
+        <div className='becomeDiv'>
+            <div>
+                <h3>Become a Creve Creative</h3>
+                <p>
+                    Showcase Your Skills and Thrive by Joining Our 
+                    Community of <br /> Talented Professionals
+                </p>
+                <button>Apply as a Creative</button>
+            </div>
+
+            <div className='becomeImgDiv'>
+                <img src={becomeImg} alt="" />
             </div>
         </div>
     </div>
