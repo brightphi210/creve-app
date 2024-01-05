b420a0b3c8c057228b949fbfeaf00c479f29f256
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-01-05 05:03:19 -0800

    latest

--- src/Components/UserDashboard/ChatCompo.jsx
+++ src/Components/UserDashboard/ChatCompo.jsx
@@ -129,9 +129,43 @@ const ChatCompo = () => {
                         </div>
                     </div>
 
-                    <div>
-                        
+
+
+
+                    <div className='messageOverFlow'>
+                        <div className='msgText'>
+                            <div className='msgTextDiv'>
+
+
+                                <div className='sendMsg sent-message'>
+                                    <div>
+                                    <p>Hello, how are you?</p>
+                                    <span>12:50pm</span>
+                                    </div>
+                                </div>
+
+
+                                <div  className='recieveMsg received-message'>
+                                    <div>
+                                    <p>I'm good, thank you!</p>
+                                    <span>12:00pm</span>
+                                    </div>
+                                </div >
+
+                                <div className='sendMsg sent-message'>
+                                    <div>
+                                    <p>Hello, how are you?</p>
+                                    <span>12:50pm</span>
+                                    </div>
+                                </div>
+
+                                
+                                
+                            </div>
+                        </div>
                     </div>
+
+
 
                     <div className='messageBottom'>
                         <div className='msgBottomInput'>
