caf5a2907b81c1a4fdd8c6cb92a5a3980903dd35
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-01-09 14:05:58 -0800

    a

--- src/Components/SignupComponent/ConfirmPageCompo.jsx
+++ src/Components/SignupComponent/ConfirmPageCompo.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import './ConfirmPageCompo.scss'
 import sentMail from './images/email.avif'
 
 const ConfirmPageCompo = () => {
+  
   return (
     <div>
-      <div>
-        <img src={sentMail} alt="" />
-        <p></p>
+      <div className='confirmDiv'>
+        <div className='confirmText'>
+          <img src={sentMail} alt="" />
+          <h2>Email Verification Sent !!</h2>
+          <p>Please Visit your mail to very your email address</p>
+          
+          <div className='contactCareDiv'>
+            <p>contact customer care if you didnt recieve a mail </p>
+            <p>+234 80944 22807</p>
+          </div>
+        </div>
       </div>
+
+      
     </div>
   )
 }
