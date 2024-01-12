ef6c574c547a42589fded78427b1cb1f0f127683
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-01-12 06:44:47 -0800

    a

--- src/Components/UserDashboard/UserSettingCompo.jsx
+++ src/Components/UserDashboard/UserSettingCompo.jsx
@@ -1,42 +1,152 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './UserSettingCompo.scss'
 
 import {Link} from 'react-router-dom'
 
 import { FaRegTrashCan } from "react-icons/fa6";
-
+import {useNavigate} from 'react-router-dom'
+import prof from './images/profilePics.png'
 
 const UserSettingCompo = () => {
+
+
+    let token = localStorage.getItem('authToken');
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate()
+
+
+    const [name, setName] = useState('')
+    const [profile_pics, setProfile_pics] = useState(null)
+
+    
+    // const handleChange = (e) => {
+    //     setProfile_pics(e.target.value);
+    // };
+
+
+    const handleImageChange = (e) => {
+        setProfile_pics(e.target.files[0]);
+    };
+
+
+    const fetchUserData = async (e) => {
+
+        setIsLoading(true)
+
+        try {
+          const response = await fetch(`https://creve.onrender.com/clientupdate/`, {
+            method: 'GET',
+            headers: {
+              "Authorization": `Bearer ${token}`,
+            //   "Content-Type": "application/json"
+            },
+          });
+
+          setIsLoading(false)
+          const userData = await response.json();
+          console.log("This is the user data: " + JSON.stringify(userData));
+
+          setName(userData.name);
+          setProfile_pics(userData.profile_pics);
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+          setIsLoading(false)
+        }
+    };
+
+    useEffect(() => {
+        fetchUserData();
+      }, []);
+
+
+
+      console.log()
+
+      function handleSubmit(e) {
+        e.preventDefault();
+
+        setIsLoading(true);
+
+        const newFormData = new FormData();
+        newFormData.append('name', name);
+        newFormData.append('profile_pics', profile_pics);
+
+      
+        fetch(`https://creve.onrender.com/clientupdate/`, {
+          method: 'PUT',
+          headers: {
+            "Authorization": `Bearer ${token}`
+          },
+          body: newFormData,
+        })
+          .then((response) => {
+            if (response.ok) {
+                console.log("Successfully updated")
+                // navigate('/dashboard')
+                setIsLoading(false)
+            } else {
+              console.log("Failed to update")
+              setIsLoading(false)
+            }
+          })
+          .catch((error) => {
+            console.error('Error updating user profile:', error);
+            setIsLoading(false)
+          });
+      }
+      
+
+
   return (
     <div className='userSetting'>
         <div className='userDiv'>
             <div className='userDivEach'>
                 <h2>Account Settings</h2>
-                <form action="">
+
+
+                <form action="" onSubmit={handleSubmit}>
+                    <hr />
+                    <div className='proImageDiv'>
+                    <img src={prof} alt="" />
+                        {isLoading ? (
+                            <>
+                                <img src={prof} alt="" />
+                            </>
+                        ) 
+                        : (
+
+                            <>
+                                <img src={profile_pics} alt=""/>
+                            </>
+                        )}
+
+                        <input 
+                            type="file" 
+                            id='image' 
+                            name="profile_pics" 
+                            accept="image/*"
+                            onChange ={handleImageChange} 
+                            hidden
+                        />
+                        <label for="image">Choose file</label>
+                    </div>
+
+                    <hr />
+
                     <div>
                         <p>Full Name: </p>
-                        <input type="text"  placeholder='Enter your Full Name'/>
+                        <input type="text"  
+                        placeholder='Enter your Full Name'
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        />
                     </div>
-                    <button>Save Changes</button>
+                    <button>{isLoading ? 'Save  . . .' : 'Saving Changes'}</button>
                 </form>
             </div>
-
-            {/* <div className='userDivEach'>
-                <h2>Change Password</h2>
-                <form action="">
-                    <div>
-                        <p>Current Password: </p>
-                        <input type="text"  placeholder='Enter Current Password'/>
-                    </div>
-
-                    <div>
-                        <p>New Password: </p>
-                        <input type="text" placeholder='Enter New Password'/>
-                    </div>
-
-                    <button>Save Changes</button>
-                </form>
-            </div> */}
 
             <hr />
 
@@ -54,6 +164,15 @@ const UserSettingCompo = () => {
 
             <p className='deleteBtn'><FaRegTrashCan />Delete Account</p>
         </div>
+
+
+
+        {isLoading ? 
+      (<>
+        <div className='loaderModal'>
+          <span className="loader"></span>
+        </div>
+      </>) : ''}
     </div>
   )
 }
